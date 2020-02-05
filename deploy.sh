d93e028fc489e3f27d224d722a6906f35beff709
IamHi <inpactt@gmail.com>
2020-02-05 20:45:34 +0100

    DEPLOY!6

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,14 @@
 docker build -t iamhi/multi-client:latest -t iamhi/multi-client:$GIT_SHA -f ./client/Dockerfile ./client
 docker build -t iamhi/multi-server:latest -t iamhi/multi-server:$GIT_SHA -f ./server/Dockerfile ./server
-docker build -t worker/multi-worker:latest -t iamhi/multi-worker:$GIT_SHA -f ./worker/Dockerfile ./worker
+docker build -t iamhi/multi-worker:latest -t iamhi/multi-worker:$GIT_SHA -f ./worker/Dockerfile ./worker
 
-docker push worker/multi-client:latest
-docker push worker/multi-server:latest
-docker push worker/multi-worker:latest
+docker push iamhi/multi-client:latest
+docker push iamhi/multi-server:latest
+docker push iamhi/multi-worker:latest
 
-docker push worker/multi-client:$GIT_SHA
-docker push worker/multi-server:$GIT_SHA
-docker push worker/multi-worker:$GIT_SHA
+docker push iamhi/multi-client:$GIT_SHA
+docker push iamhi/multi-server:$GIT_SHA
+docker push iamhi/multi-worker:$GIT_SHA
 
 kubectl apply -f k8s
 kubectl set image deployments/server-deployment server=iamhi/multi-server:$GIT_SHA
